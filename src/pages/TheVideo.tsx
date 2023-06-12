1cdf1fd44793c472dcc9c755adaa4bb093a6df59
guilhermevialle <vialleguilherme@gmail.com>
2023-06-11 21:45:55 -0300

    fixing download options layout

--- src/pages/TheVideo.tsx
+++ src/pages/TheVideo.tsx
@@ -38,94 +38,107 @@ export default function TheVideo({ id, video, formats }: Props) {
   return (
     <main className='w-screen h-screen'>
       <Padding stretch={true}>
-        <div className='my-6 space-y-6'>
-          <Search />
-        </div>
-        <div className='w-full'>
-          <VideoCard details={video} />
-
-          <div className='flex justify-between items-center  mt-8 mb-3 '>
-            <h1 className='text-lg font-medium'>Download options</h1>
-            <Dropdown
-              buttonTitle='Audio'
-              sectionTitle='Choose type format'
-              buttonSize='w-[120px]'
-              sectionSize='w-[180px]'
-              options={[
-                {
-                  text: 'Audio',
-                  clickFn: () => setFormatTypeOption('Audio'),
-                },
-                {
-                  text: 'Video',
-                  clickFn: () => setFormatTypeOption('Video'),
-                },
-                {
-                  text: 'Full',
-                  clickFn: () => setFormatTypeOption('Full'),
-                },
-              ]}
-            />
+        <div className='w-full h-full'>
+          <div className='w-full h-[50%]'>
+            <div className='my-6'>
+              <Search />
+            </div>
+            <div className='w-full'>
+              <VideoCard details={video} />
+            </div>
+            <div className='flex justify-between items-center  mt-8 mb-3 '>
+              <h1 className='text-lg font-medium'>Download options</h1>
+              <Dropdown
+                buttonTitle='Audio'
+                sectionTitle='Choose type format'
+                buttonSize='w-[120px]'
+                sectionSize='w-[180px]'
+                options={[
+                  {
+                    text: 'Audio',
+                    clickFn: () => setFormatTypeOption('Audio'),
+                  },
+                  {
+                    text: 'Video',
+                    clickFn: () => setFormatTypeOption('Video'),
+                  },
+                  {
+                    text: 'Full',
+                    clickFn: () => setFormatTypeOption('Full'),
+                  },
+                ]}
+              />
+            </div>
           </div>
 
-          {option == 'Audio' ? (
-            <div className='w-full'>
-              <h1 className='text-base text-neutral-300 mb-2'>Audio formats</h1>
-              <div className='w-full h-[200px] overflow-y-auto py-2 space-y-2'>
-                {formats.audios?.map((format) => {
-                  return (
-                    <Format
-                      video={video}
-                      format={format}
-                      key={v4()}
-                      fields={{
-                        first: format.audioBitrate + 'k',
-                        second: megabytes(Number(format.contentLength)) + 'MB',
-                      }}
-                    />
-                  )
-                })}
+          <div className='w-full h-[50%]'>
+            {option == 'Audio' ? (
+              <div className='w-full h-full'>
+                <h1 className='text-base text-neutral-300 mb-2'>
+                  Audio formats
+                </h1>
+                <div className='w-full h-full overflow-y-auto py-2 space-y-2'>
+                  {formats.audios?.map((format) => {
+                    return (
+                      <Format
+                        video={video}
+                        format={format}
+                        key={v4()}
+                        fields={{
+                          first: format.audioBitrate + 'k',
+                          second:
+                            megabytes(Number(format.contentLength)) + 'MB',
+                        }}
+                      />
+                    )
+                  })}
+                </div>
               </div>
-            </div>
-          ) : option == 'Video' ? (
-            <div className='w-full'>
-              <h1 className='text-base text-neutral-300 mb-2'>Video formats</h1>
-              <div className='w-full h-[200px] overflow-y-auto py-2 space-y-2'>
-                {formats.videos?.map((format) => {
-                  return (
-                    <Format
-                      key={v4()}
-                      video={video}
-                      format={format}
-                      fields={{
-                        first: format.qualityLabel,
-                        second: megabytes(Number(format.contentLength)) + 'MB',
-                      }}
-                    />
-                  )
-                })}
+            ) : option == 'Video' ? (
+              <div className='w-full h-full'>
+                <h1 className='text-base text-neutral-300 mb-2'>
+                  Video formats
+                </h1>
+                <div className='w-full h-full overflow-y-auto py-2 space-y-2'>
+                  {formats.videos?.map((format) => {
+                    return (
+                      <Format
+                        key={v4()}
+                        video={video}
+                        format={format}
+                        fields={{
+                          first: format.qualityLabel,
+                          second:
+                            megabytes(Number(format.contentLength)) + 'MB',
+                        }}
+                      />
+                    )
+                  })}
+                </div>
               </div>
-            </div>
-          ) : (
-            <div className='w-full'>
-              <h1 className='text-base text-neutral-300 mb-2'>Full formats</h1>
-              <div className='w-full h-[200px] overflow-y-auto py-2 space-y-2'>
-                {formats.full?.map((format) => {
-                  return (
-                    <Format
-                      key={v4()}
-                      video={video}
-                      format={format}
-                      fields={{
-                        first: format.qualityLabel,
-                        second: format.quality,
-                      }}
-                    />
-                  )
-                })}
+            ) : (
+              <div className='w-full h-full'>
+                <h1 className='text-base text-neutral-300 mb-2'>
+                  Full formats
+                </h1>
+                <div className='w-full h-full overflow-y-auto py-2 space-y-2'>
+                  {formats.full?.map((format) => {
+                    return (
+                      <Format
+                        key={v4()}
+                        video={video}
+                        format={format}
+                        fields={{
+                          first: format.qualityLabel,
+                          second: format.quality,
+                        }}
+                      />
+                    )
+                  })}
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </Padding>
     </main>
