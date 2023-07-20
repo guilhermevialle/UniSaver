cba47c2cf0369cb5d111614e2a8ef720c48d33b7
guilhermevialle <vialleguilherme@gmail.com>
2023-07-20 09:26:40 -0300

    changing some styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className='w-full h-fit flex items-center mt-6'>
+    <footer className='w-full h-fit flex items-center my-6'>
       <h1 className='w-full text-sm font-medium text-neutral-500 text-center'>
         © 2023 Youtube Catcher. All Rights Reserved.
       </h1>
